c8009eca51dce03f443cc1604fa40d75a85230cc
Christopher Nokes <50048038+cnokes14@users.noreply.github.com>
2023-03-07 16:25:31 -0500

    Removed shift digit dependence

--- CPU_Files/srlN.vhd
+++ CPU_Files/srlN.vhd
@@ -19,10 +19,9 @@ use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
 entity srlN is
-	GENERIC ( N : INTEGER := 4; -- bit width
-		      M : INTEGER := 2); -- shift bits
+	GENERIC ( N : INTEGER := 4); -- bit width
 	PORT ( A : IN std_logic_vector (N-1 downto 0) ;
-	       SHIFT_AMT : IN std_logic_vector (M-1 downto 0) ;
+	       SHIFT_AMT : IN std_logic_vector (N-1 downto 0) ;
 	       Y : OUT std_logic_vector (N-1 downto 0)
 	);
 end srlN;
@@ -39,6 +38,12 @@ begin
 			aSRL ( i ) (N-1 downto N-i) <= ( others => '0') ;
 		end generate left_fill;
 	end generate generateSRL;
-	Y <= aSRL ( to_integer ( unsigned ( SHIFT_AMT ) ) ) when
-		      ( to_integer ( unsigned ( SHIFT_AMT ) ) < N) else ( others => '0') ;
+	
+	
+	out_set : process (aSRL, SHIFT_AMT, A) is begin
+	   if to_integer(unsigned(SHIFT_AMT)) > N-1 or to_integer(unsigned(SHIFT_AMT)) < 0 then
+	       Y <= (others => '0');
+	   else Y <= aSRL(to_integer(unsigned(SHIFT_AMT)));
+	   end if;
+    end process out_set;
 end behavioral;
