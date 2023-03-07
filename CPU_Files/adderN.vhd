553c825efa924c187ae61c247e42798e018ad874
Christopher Nokes <50048038+cnokes14@users.noreply.github.com>
2023-03-07 16:26:50 -0500

    Removed redefined fullAdder component

--- CPU_Files/adderN.vhd
+++ CPU_Files/adderN.vhd
@@ -2,20 +2,6 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity fullAdder is port(
-    A,B,C : IN STD_LOGIC;
-    Sum, Cout : OUT STD_LOGIC);
-end fullAdder;
-
-architecture behavioral of fullAdder is begin
-    Cout <= (A and B) or (B and C) or (A and C);
-    Sum <= A xor B xor C;
-end behavioral;
-
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-
 entity adderN is
     generic(width : Integer := 8);
     port(A, B : IN STD_LOGIC_VECTOR(width-1 downto 0);
